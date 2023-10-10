969ae9b9b18e853002fe773902dd7e6bacbe9ef8
Gaya3DeviK <147460598+Gaya3DeviK@users.noreply.github.com>
2023-10-09 20:17:38 -0600

    Add files via upload

---
+++ Lab008.py
@@ -0,0 +1,12 @@
+#How to take an input
+
+Firstname = input("Enter your firstname")
+print(Firstname)
+
+Lastname = input("Enter your Lastname")
+print(Lastname)
+
+print(Firstname,Lastname,sep="-",end="\t")
+print('\n')
+name = input("Enter your name")
+print("You are welcome",name,"to the Pyton class")
